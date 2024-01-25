5167fb10210a47aaa31aceb85b3c6063a3830fc7
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-01-25 19:51:57 +0530

    .

--- currencyproject/src/App.jsx
+++ currencyproject/src/App.jsx
@@ -1,10 +1,30 @@
-import { useState, useId } from "react";
+import { useState, useId, useEffect } from "react";
 import InputBox from "./assets/components/InputBox";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch(
+      "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json"
+    )
+      .then((res) => {
+        // console.log(res, "hello", res.json());
+        return res.json();
+      })
+      .then((res) => {
+        let data = res.usd;
+        let newdata = Object.keys(data);
+        // console.log(newdata, data, "hello");
+        setData(newdata);
+      });
+  }, []);
+
+  //nakli
+  const [currencyOpt, setCurrencyOpt] = useState([]);
+
   const [amount, setAmount] = useState(0);
+  const [data, setData] = useState([]);
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("inr");
   const [ConvertedAmount, setConvertedAmount] = useState(0);
@@ -21,6 +41,25 @@ function App() {
 
   function Convert() {
     setConvertedAmount(amount * CurrencyInfo[to]);
+  }
+  function handleSearch(val) {
+    let filterdrst = data.filter((item) => item.includes(val));
+    if (val.length < 1) {
+      setCurrencyOpt([]);
+    } else {
+      for (let i in filterdrst) {
+        console.log(filterdrst[i], i);
+        if (filterdrst[i] == val) {
+          console.log("yes");
+          let temvalue = filterdrst[i];
+          filterdrst.splice(i, 1);
+          filterdrst.unshift(temvalue);
+          setCurrencyOpt(filterdrst);
+        }
+      }
+      setCurrencyOpt(filterdrst);
+    }
+    console.log(val);
   }
 
   return (
@@ -76,6 +115,23 @@ function App() {
           </form>
         </div>
       </div>
+      {/* <div className="h-48 w-full bg-slate-400 flex flex-col items-center justify-center overflow-hidden">
+        <input
+          type="search"
+          onChange={(e) => handleSearch(e.target.value)}
+          name=""
+          id=""
+          placeholder="search your currency"
+        />
+        <div className="overflow-y-auto h-64 w-60 bg-slate-400">
+          {currencyOpt.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </div>
+        
+      </div> */}
     </div>
   );
 }
